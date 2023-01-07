ad8079b2ed7a8546a4afd02f414398cdb06f8165
Marko-air <m.niinimaki@myfriendapp.com>
2023-01-07 12:17:33 +0700

    index

--- dataspace.py
+++ dataspace.py
@@ -623,7 +623,7 @@ def fromwiki():
                 while os.path.isfile(app.config['SL']+checkfilename):
                     countn += 1
                     checkfilename = "data"+str(countn)+".csv"
-                dataf.to_csv(app.config['SL']+checkfilename)
+                dataf.to_csv(app.config['SL']+checkfilename, index=None)
                 numlines = dataf.shape[0]
                 fieldlist = build_fieldlist(app.config['SL']+checkfilename)
                 return render_template('editmeta.html', file=checkfilename, descr="",\
